d60f864159e3173f16b0437ea67fb7e60da7e9b2
bruno yang <ygch1993@gmail.com>
2015-04-26 22:47:44 +0800

    news center

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -2,6 +2,20 @@ var mongoose = require('mongoose');
 var News = mongoose.model('News');
 var _ = require('underscore');
 
+exports.news = function(req, res) {
+  var start = req.query.start ? req.query.start : 0;
+  var limit = req.query.limit ? req.query.limit : 15;
+  News.fetchLimit(start, limit, function(err, newslist){
+    if(err) {
+      console.log(err);
+    }
+    res.render('frontend/news/news', {
+      title: '新闻列表',
+      newslist: newslist
+    });
+  });
+};
+
 exports.detail = function(req, res) {
   var id = req.params.id;
 
@@ -41,7 +55,7 @@ exports.save = function(req, res) {
           console.log(err);
         }
 
-        res.redirect('/news/' + news._id);
+        res.redirect('/admin/news/list');
       });
     });
   } else {
@@ -55,7 +69,7 @@ exports.save = function(req, res) {
         console.log(err);
       }
 
-      res.redirect('/news/' + news._id);
+      res.redirect('/admin/news/list');
     });
   }
 };
